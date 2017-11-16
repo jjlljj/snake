c1b2a6be394e54bb504511588828d10a317c03a7
James Logue <logueja@gmail.com>
2017-11-15 19:14:38 -0700

    Update snake motion and frame speed

--- scripts/game.js
+++ scripts/game.js
@@ -2,17 +2,19 @@ function Game(screenId) {
   var canvas = document.getElementById(screenId);
   var screen = canvas.getContext('2d');
   var gameSize = {x: canvas.width, y: canvas.height};
-  console.log(gameSize)
+  var frameLength = 300;
 
   this.bodies = [new Snake(this, gameSize)];
 
   var self = this;
   var tick = function() {
+    setInterval(function() {
     self.update();
     self.draw(screen, gameSize);
-    requestAnimationFrame(tick);
+    }, frameLength);
   };
-  tick();
+
+  tick();  
 }
 
 Game.prototype.update = function() {
@@ -20,7 +22,7 @@ Game.prototype.update = function() {
   for (var i = 0; i < this.bodies.length; i++) {
     this.bodies[i].update();
   };
-}
+};
 
 Game.prototype.draw = function(screen, gameSize) {
   screen.clearRect(0, 0, gameSize.x, gameSize.y);
@@ -28,56 +30,66 @@ Game.prototype.draw = function(screen, gameSize) {
   this.bodies[0].blocks.forEach(function(block) {
     drawRect(screen, block)
   });
-}
+};
 
 function Snake(game, gameSize) {
   this.game = game;
   this.blocks = [];
+  this.direction = 'up';
   this.head = {x: gameSize.x / 2, y: gameSize.y / 2};
   this.blocks.push(new SnakeBlock(game, this.head))
   this.keyboarder = new Keyboarder();
-  
-}
+};
+
+Snake.prototype.setDirection = function() {
+  if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT) && this.head.x > 15) {
+    this.direction = 'left';
+  } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT) && this.head.x < 305) {
+    this.direction = 'right';
+  } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP) && this.head.y > 15) {
+    this.direction = 'up';
+  } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN) && this.head.y < 305) {
+    this.direction = 'down';
+  };
+};
 
 Snake.prototype.update = function() {
-   if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT) && this.head.x > 15) {
+  this.setDirection();
+
+   if (this.direction === 'left' && this.head.x > 15) {
     this.head = {x: this.head.x - 15, y: this.head.y}
     this.blocks.unshift(new SnakeBlock(this.game, this.head));
     this.blocks.pop();
-  } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT) && this.head.x < 305) {
+  } else if (this.direction === 'right' && this.head.x < 305) {
     this.head = {x: this.head.x + 15, y: this.head.y}
     this.blocks.unshift(new SnakeBlock(this.game, this.head));
     this.blocks.pop();
-  } else if (this.keyboarder.isDown(this.keyboarder.KEYS.UP) && this.head.y > 15) {
+  } else if (this.direction === 'up' && this.head.y > 15) {
     this.head = {x: this.head.x, y: this.head.y - 15}
     this.blocks.unshift(new SnakeBlock(this.game, this.head));
     this.blocks.pop();
-  } else if (this.keyboarder.isDown(this.keyboarder.KEYS.DOWN) && this.head.y < 305) {
+  } else if (this.direction === 'down' && this.head.y < 305) {
     this.head = {x: this.head.x, y: this.head.y + 15}
     this.blocks.unshift(new SnakeBlock(this.game, this.head));
     this.blocks.pop();
-  } 
-}
+  };
+};
 
-// Snake.prototype.addBlock = function(block) {
-//   this.blocks.push(body);
-// }
+Snake.prototype.addBlock = function(block) {
+  this.blocks.push(body);
+};
 
 function SnakeBlock(game, center) {
   this.game = game;
   this.size = {x: 15, y: 15};
   this.center = center;
-}
-
-
+};
 
 function drawRect(screen, body) {
   screen.fillRect(body.center.x - body.size.x / 2,
                     body.center.y - body.size.y / 2,
                     body.size.x, body.size.y);
 };
-
-
 
 function Keyboarder() {
   var keyState = {};
@@ -97,4 +109,4 @@ function Keyboarder() {
 
 window.onload = function() {
   new Game('screen');
-}
+};
